a93fc65832102a6dbfcd78dfc5c8dba91f8684a8
taohuifei@gmail.com <taohuifei@gmail.com@500b721a-f10f-577f-f222-ee804c352ab6>
2012-09-26 11:04:32 +0000

    当同一连接下，收到多个包时，原来处理包一个个触发IO事件的，现在改为将所有包一次发个线程池，减少IO线程切换

--- trunk/src/code/google/dsf/protocol/NettyTransportCodec.java
+++ trunk/src/code/google/dsf/protocol/NettyTransportCodec.java
@@ -1,5 +1,6 @@
 package code.google.dsf.protocol;
 
+import java.net.SocketAddress;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
@@ -8,7 +9,11 @@ import org.jboss.netty.buffer.ChannelBuffer;
 import org.jboss.netty.buffer.ChannelBuffers;
 import org.jboss.netty.channel.Channel;
 import org.jboss.netty.channel.ChannelHandlerContext;
-import org.jboss.netty.handler.codec.frame.FrameDecoder;
+import org.jboss.netty.channel.ChannelStateEvent;
+import org.jboss.netty.channel.Channels;
+import org.jboss.netty.channel.ExceptionEvent;
+import org.jboss.netty.channel.MessageEvent;
+import org.jboss.netty.channel.SimpleChannelUpstreamHandler;
 import org.jboss.netty.handler.codec.oneone.OneToOneEncoder;
 
 
@@ -24,6 +29,7 @@ public class NettyTransportCodec {
 
   /**
    * 编码
+   * 
    * @author taohuifei
    */
   public static class NettyFrameEncoder extends OneToOneEncoder {
@@ -76,7 +82,167 @@ public class NettyTransportCodec {
   /**
    * 解码
    */
-  public static class NettyFrameDecoder extends FrameDecoder {
+  public static class NettyFrameDecoder  extends SimpleChannelUpstreamHandler {
+
+    private final boolean unfold;
+    private ChannelBuffer cumulation;
+
+    public NettyFrameDecoder() {
+        this(false);
+    }
+
+    public NettyFrameDecoder(boolean unfold) {
+        this.unfold = unfold;
+    }
+
+    @Override
+    public void messageReceived(
+            ChannelHandlerContext ctx, MessageEvent e) throws Exception {
+
+        Object m = e.getMessage();
+        if (!(m instanceof ChannelBuffer)) {
+            ctx.sendUpstream(e);
+            return;
+        }
+
+        ChannelBuffer input = (ChannelBuffer) m;
+        if (!input.readable()) {
+            return;
+        }
+
+        ChannelBuffer cumulation = cumulation(ctx);
+        if (cumulation.readable()) {
+            cumulation.discardReadBytes();
+            cumulation.writeBytes(input);
+            callDecode(ctx, e.getChannel(), cumulation, e.getRemoteAddress());
+        } else {
+            callDecode(ctx, e.getChannel(), input, e.getRemoteAddress());
+            if (input.readable()) {
+                cumulation.writeBytes(input);
+            }
+        }
+    }
+
+    @Override
+    public void channelDisconnected(
+            ChannelHandlerContext ctx, ChannelStateEvent e) throws Exception {
+        cleanup(ctx, e);
+    }
+
+    @Override
+    public void channelClosed(
+            ChannelHandlerContext ctx, ChannelStateEvent e) throws Exception {
+        cleanup(ctx, e);
+    }
+
+    @Override
+    public void exceptionCaught(
+            ChannelHandlerContext ctx, ExceptionEvent e) throws Exception {
+        ctx.sendUpstream(e);
+    }
+
+    /**
+     * Decodes the received data so far into a frame when the channel is
+     * disconnected.
+     *
+     * @param ctx      the context of this handler
+     * @param channel  the current channel
+     * @param buffer   the cumulative buffer of received packets so far.
+     *                 Note that the buffer might be empty, which means you
+     *                 should not make an assumption that the buffer contains
+     *                 at least one byte in your decoder implementation.
+     *
+     * @return the decoded frame if a full frame was received and decoded.
+     *         {@code null} if there's not enough data in the buffer to decode a frame.
+     */
+    protected Object decodeLast(
+            ChannelHandlerContext ctx, Channel channel, ChannelBuffer buffer) throws Exception {
+        return decode(ctx, channel, buffer);
+    }
+
+    private void callDecode(
+            ChannelHandlerContext context, Channel channel,
+            ChannelBuffer cumulation, SocketAddress remoteAddress) throws Exception {
+        List<Object> results = new ArrayList<Object>();
+        while (cumulation.readable()) {
+            int oldReaderIndex = cumulation.readerIndex();
+            Object frame = decode(context, channel, cumulation);
+            if (frame == null) {
+                if (oldReaderIndex == cumulation.readerIndex()) {
+                    // Seems like more data is required.
+                    // Let us wait for the next notification.
+                    break;
+                } else {
+                    // Previous data has been discarded.
+                    // Probably it is reading on.
+                    continue;
+                }
+            } else if (oldReaderIndex == cumulation.readerIndex()) {
+                throw new IllegalStateException(
+                        "decode() method must read at least one byte " +
+                        "if it returned a frame (caused by: " + getClass() + ")");
+            }
+            results.add(frame);
+        }
+        if(results.size() > 0)
+            unfoldAndFireMessageReceived(context, remoteAddress, results);
+    }
+
+    private void unfoldAndFireMessageReceived(ChannelHandlerContext context, SocketAddress remoteAddress, Object result) {
+        if (unfold) {
+            if (result instanceof Object[]) {
+                for (Object r: (Object[]) result) {
+                    Channels.fireMessageReceived(context, r, remoteAddress);
+                }
+            } else if (result instanceof Iterable<?>) {
+                for (Object r: (Iterable<?>) result) {
+                    Channels.fireMessageReceived(context, r, remoteAddress);
+                }
+            } else {
+                Channels.fireMessageReceived(context, result, remoteAddress);
+            }
+        } else {
+            Channels.fireMessageReceived(context, result, remoteAddress);
+        }
+    }
+
+    private void cleanup(ChannelHandlerContext ctx, ChannelStateEvent e)
+            throws Exception {
+        try {
+            ChannelBuffer cumulation = this.cumulation;
+            if (cumulation == null) {
+                return;
+            } else {
+                this.cumulation = null;
+            }
+
+            if (cumulation.readable()) {
+                // Make sure all frames are read before notifying a closed channel.
+                callDecode(ctx, ctx.getChannel(), cumulation, null);
+            }
+
+            // Call decodeLast() finally.  Please note that decodeLast() is
+            // called even if there's nothing more to read from the buffer to
+            // notify a user that the connection was closed explicitly.
+            Object partialFrame = decodeLast(ctx, ctx.getChannel(), cumulation);
+            if (partialFrame != null) {
+                unfoldAndFireMessageReceived(ctx, null, partialFrame);
+            }
+        } finally {
+            ctx.sendUpstream(e);
+        }
+    }
+
+    private ChannelBuffer cumulation(ChannelHandlerContext ctx) {
+        ChannelBuffer c = cumulation;
+        if (c == null) {
+            c = ChannelBuffers.dynamicBuffer(
+                    ctx.getChannel().getConfig().getBufferFactory());
+            cumulation = c;
+        }
+        return c;
+    }
+
 
     protected Object decode(ChannelHandlerContext ctx, Channel channel, ChannelBuffer in)
         throws Exception {
--- trunk/src/code/google/dsf/server/IProtocolHandler.java
+++ trunk/src/code/google/dsf/server/IProtocolHandler.java
@@ -4,11 +4,14 @@ import java.nio.ByteBuffer;
 import java.util.List;
 
 import code.google.dsf.protocol.ProtocolPack;
+import code.google.dsf.protocol.rpc.RPCMetaDTO;
 
 public interface IProtocolHandler {
   
   public void registerProcessor(String beanName, Object instance);
 
   public List<ByteBuffer> handleRequest(ProtocolPack dataPack);
+  
+  public List<ByteBuffer> putRespond(Object result, byte contentType, RPCMetaDTO rcpMeta);
   
 }
